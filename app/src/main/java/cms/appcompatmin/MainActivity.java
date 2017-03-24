62cdbc2971663e5ffb66ee64ecafee1efed03776
wenyuan.chi <wenyuan.chi@ileopard.com>
2017-03-24 11:08:52 +0800

    init. empty app

---
+++ app/src/main/java/cms/appcompatmin/MainActivity.java
@@ -0,0 +1,16 @@
+package cms.appcompatmin;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by wychi on 2017/3/24.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
